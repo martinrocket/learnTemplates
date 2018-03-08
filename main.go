302277ae24e455894dab5f10c17d5c3c7d7d3d8e
martinrocket <martin.ruble@gmx.com>
2018-03-07 19:13:43 -0700

    change map to struct, add button data to main.go, tmpl.css and tmpl1.html

--- main.go
+++ main.go
@@ -8,28 +8,36 @@ import (
 	"time"
 )
 
+type webContent struct {
+	Footer string
+	Header string
+	Button template.HTML
+}
+
 func main() {
 	ti := time.Now()
 
-	//ti.Format("Mon Jan 01-02-2006 15:04 PM")
-
 	myTimeStr := "footer - " + ti.Format("Mon Jan 02 2006 15:04 PM") + " - footer"
-
-	//hf := []string{"my header from slice", "my footer from slice"}
-	hf1 := map[string]string{
-
-		"footer": myTimeStr,
-		"header": "header from map!",
+	myWebContent := webContent{
+		Footer: myTimeStr,
+		Header: "my new header",
+		Button: template.HTML("my button"),
 	}
+	//hf := []string{"my header from slice", "my footer from slice"}
+	// hf1 := map[string]string{
+
+	// 	"footer": myTimeStr,
+	// 	"header": "header from map!",
+	// }
 
 	t, err := template.ParseFiles("tmpl1.html")
 	fmt.Printf("Parsed file %v with error = %v.\n", t.Name(), err)
-	t.Execute(os.Stdout, hf1)
+	t.Execute(os.Stdout, myWebContent)
 
 	f, err := os.Create("output.html")
 	defer f.Close()
 	fmt.Printf("\nCreated the file %v with error = %v.\n", f.Name(), err)
-	t.Execute(f, hf1)
-	fmt.Println(hf1)
+	t.Execute(f, myWebContent)
+	fmt.Println(myWebContent)
 
 }
